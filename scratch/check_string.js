96aeeb59224ef6659013c2f5a33bb41fff6b3453
Norberto Martín Afonso <normaafo@gmail.com>
2026-05-01 09:34:49 +0100

    Final cleanup of encoding debug files

--- scratch/check_string.js
+++
@@ -1,6 +0,0 @@
-
-const spanishLetters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-console.log("Length:", spanishLetters.length);
-for (let i = 0; i < spanishLetters.length; i++) {
-    console.log(`Pos ${i}: ${spanishLetters[i]} (code: ${spanishLetters.charCodeAt(i)})`);
-}
